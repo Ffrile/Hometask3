cdbdbba2ce2b01e827aa82cf8b87ab869d37e7b3
Zhenya <wowkylgame@gmail.com>
2023-01-26 18:33:46 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num;
+int num2 = 0;
+while (num > 0)
+{
+    num2 = (num2 *10) + num % 10;
+    num = num / 10; 
+}
+if (num2 == num1)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Не палиндром");
+}
